f773227a22b6f2498f15e02e82e43ed4abf94526
Heavenell <Fauzirahmadiariateja@gmail.com>
2024-09-29 16:33:04 +0200

    add the data to api.js
    later i will move it to a .json file or something

--- backend/api.js
+++ backend/api.js
@@ -1,6 +1,18 @@
-
-
-
+const FLASHCARDS = [
+    { "kanji": "一", "english": "One", "onyomi": ["ichi", "itsu"], "kunyomi": ["hito"], "toggle": "false" },
+    { "kanji": "二", "english": "Two", "onyomi": ["ni"], "kunyomi": ["futa"], "toggle": "false" },
+    { "kanji": "三", "english": "Three", "onyomi": ["san"], "kunyomi": ["mi"], "toggle": "false" },
+    { "kanji": "四", "english": "Four", "onyomi": ["shi"], "kunyomi": ["yon", "yo"], "toggle": "false" },
+    { "kanji": "五", "english": "Five", "onyomi": ["go"], "kunyomi": ["itsu"], "toggle": "false" },
+    { "kanji": "六", "english": "Six", "onyomi": ["roku"], "kunyomi": ["mu"], "toggle": "false" },
+    { "kanji": "七", "english": "Seven", "onyomi": ["shichi"], "kunyomi": ["nana"], "toggle": "false" },
+    { "kanji": "八", "english": "Eight", "onyomi": ["hachi"], "kunyomi": ["ya"], "toggle": "false" },
+    { "kanji": "九", "english": "Nine", "onyomi": ["kyuu", "ku"], "kunyomi": ["kokono"], "toggle": "false" },
+    { "kanji": "十", "english": "Ten", "onyomi": ["juu"], "kunyomi": ["too"], "toggle": "false" },
+    { "kanji": "百", "english": "Hundred", "onyomi": ["hyaku"], "kunyomi": [], "toggle": "false" },
+    { "kanji": "千", "english": "Thousand", "onyomi": ["sen"], "kunyomi": ["chi"], "toggle": "false" },
+    { "kanji": "万", "english": "Ten Thousand", "onyomi": ["man", "ban"], "kunyomi": [], "toggle": "false" },
+];
 
 
 module.exports = {
